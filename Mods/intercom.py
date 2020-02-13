0947b4807cc1a3a0f321866f591276e125c37f86
Carlos Saucedo <carlos@cgsphoto.com>
2020-02-13 18:16:08 -0500

    Fixed Teams bug. `fetchone[0]` to `fetchone()[0]`

--- Mods/intercom.py
+++ Mods/intercom.py
@@ -62,7 +62,7 @@ class Client(object):
         else:
             # Fetch the conversation ID.
             c.execute("SELECT convoId FROM users WHERE userId='"+userId+"';")
-            convoId = c.fetchone[0]
+            convoId = c.fetchone()[0]
             if(convoId != None):
                 # If the conversation is active.
                 self.client.conversations.reply(
